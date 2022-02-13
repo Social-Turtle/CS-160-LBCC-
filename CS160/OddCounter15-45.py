4fe2bc36544a59fadcec012ab888d2429a0ef7be
Social Turtle <52430191+Social-Turtle@users.noreply.github.com>
2022-02-12 19:10:45 -0800

    Update and rename Program2.py to OddCounter15-45.py

--- CS160/OddCounter15-45.py
+++ CS160/OddCounter15-45.py
@@ -4,6 +4,6 @@ print("Hello, and welcome to the odd number 15 to 45 counter!")
 i = 15
 #defining our numeric variable, i
 
-while i<=45: #count from 22 to 44 with a step of 2
+while i<=45: #count from 15 to 45 with a step of 2
     print(i) #printing our number
     i += 2
